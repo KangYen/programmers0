b1f7c4ee7922686c290b868c860fded33e45d4d9
ygang8549@gmail.com <wjsry1emd@@>
2023-02-07 16:31:14 +0900

    프로그래머스 나이 출력

---
+++ src/Age.java
@@ -0,0 +1,9 @@
+class Age {
+    public static void main(String[] args) {
+    int result = solution(40);
+        System.out.println(result);
+    }
+    static int solution(int age) {
+        return 2023-age;
+    }
+}
